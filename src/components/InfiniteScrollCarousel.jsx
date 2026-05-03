539723b140b2de1fbc92161feff7e744407ea8de
snehasingh-25 <sneha251104@gmail.com>
2026-05-03 14:26:27 +0530

    fix error

--- src/components/InfiniteScrollCarousel.jsx
+++ src/components/InfiniteScrollCarousel.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { INFINITE_SCROLL_CAROUSEL_UI } from "./infiniteScrollCarouselPresets";
 import CarouselArrow from "./CarouselArrow";
